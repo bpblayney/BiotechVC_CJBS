f57a805d83f5837c27914ed4043add1cf7a66da2
bpblayney <bpblayney@hotmail.co.uk>
2018-07-30 18:17:42 +0100

    test commit (changed file_loc to my file_loc)

--- file1.py
+++ file1.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
-file_loc = "C:/Users/pc/Desktop/payme1.csv"
+
+print("this should print now")
+file_loc = "C:/Users/bpbla/OneDrive/Documents/Cambridge/BiotechVC_CJBS/payme1.csv"
 df = pd.read_csv(file_loc)
 
 df=df[(~df['this one'].str.contains('me'))]
-#df=df[df['test'].str.contains('abc') & ]
+# df=df[df['test'].str.contains('abc') & ]
 print(df)
