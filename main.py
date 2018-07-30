e1b61b12dcaeb728a38d94e7c472b818edb55304
bpblayney <bpblayney@hotmail.co.uk>
2018-07-30 19:04:40 +0100

    linked

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # This is the main file!
+
+import pandas as pd
+import numpy as np
+import sys, os
+
+print("this should print now")
+sys.path.append(os.path.realpath('..'))
+dirname = sys.path.append(os.path.realpath('..'))
+df = pd.read_excel('Biotech companies.xls', skiprows=7)
+
+print(df.head())
