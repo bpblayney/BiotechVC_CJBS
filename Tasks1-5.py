fafa9c59f8cf53527c8e17f700a66ef3472b3713
bpblayney <bpblayney@hotmail.co.uk>
2018-08-07 13:53:44 +0100

    Tasks1-5 is new main file

--- Tasks1-5.py
+++ Tasks1-5.py
@@ -7,72 +7,58 @@ import seaborn as sns
 import nltk
 ## nltk.download()
 
+## IDEAS
 #Check out PyLucene/EnglishAnalyzer for text analysis...
 #Try LSI - latent semantic analysis using nltk in python
 #Try ML for clustering/classification? Naive Bayes...
 #Trial + Error to improve classifications
 
-
-
-
-#Import data
-#file_loc = "C:/Users/pc/Desktop/Btech.csv"
-#file_loc2 = "C:/Users/pc/Desktop/Btech2.csv"
-#df1 = pd.read_csv(file_loc, header=2,encoding='latin-1')
-#df2 = pd.read_csv(file_loc2, header=2,encoding='latin-1')
-#df=pd.concat([df1, df2])
-
 sys.path.append(os.path.realpath('..'))
-dirname = sys.path.append(os.path.realpath('..'))
+#dirname = sys.path.append(os.path.realpath('..'))
 dfB1 = pd.read_excel('Biotech companies.xls', skiprows=7)
 dfB2 = pd.read_excel('Biotech companies-2.xls', skiprows=7)
 dfEd = pd.read_excel('Educational Background.xls')
-dfB = pd.concat([dfB1, dfB2])
-df = dfB
+df = pd.concat([dfB1, dfB2])
 
-# Keyword search function
-df=df[~df['Business Description'].str.contains('LLC')]
-df=df[~df['Business Description'].str.contains('Institute', case=False)]
-df=df[~df['Business Description'].str.contains('Services')] # Too many false positves if case insenesitive
-df=df[~df['Business Description'].str.contains('Cannabis', case=False)]
-df=df[~df['Business Description'].str.contains('Consulting', case=False)] # check case sensitivity
+# ----- Top-Level Filtering -----
+filterOut_caseSensitive = ['LLC', 'Services']
+filterOut = ['Institute', 'Cannabis', 'Consulting']
+df = df[[not(any(x in s for x in filterOut_caseSensitive)) for s in df['Business Description']]]
+df = df[[not(any(x.lower() in s.lower() for x in filterOut)) for s in df['Business Description']]]
 
+# ----- Remove Subsidiaries -----
+def Remove_Subsidiaries(df):
+    # Get first word of company name to match to parent company
+    first_word = df['Company Name'].str.split().str[0] # Note: some company names have commas after - still works but cleaner to remove punctuation
+    result = pd.concat([df, first_word], axis=1) # Combine dataframes
+    result.columns.values[-1] = "For Match" # Change final column name
+    
+    # Find matches
+    result['Success']=[x[0] in x[1] for x in zip(result['For Match'], result['Parent Company'])]
+    result['Success'] = result['Success'].apply(str)
+    
+    # Remove matches (remove where both first word matches and is operating subsidiary)
+    result = result[~(result['Company Status'].str.contains('Operating Subsidiary') & result['Success'].str.contains('True'))]
+    return result
 
-# Get first word of company name to match to parent company
-first_word = df['Company Name'].str.split().str[0]
-# Combine dataframes
-result = pd.concat([df, first_word], axis=1)
-# Change final column name
-result.columns.values[-1] = "For Match"
+df = Remove_Subsidiaries(df)
 
-# Find matches
-result['Success']=[x[0] in x[1] for x in zip(result['For Match'], result['Parent Company'])]
-result['Success'] = result['Success'].apply(str)
-
-# Remove matches (remove where both first word matches and is operating subsidiary)
-result=result[~(result['Company Status'].str.contains('Operating Subsidiary') & result['Success'].str.contains('True'))]
-
-# Plot Biotech companies
-for_plot_T1 = result['Year Founded']
+# ----- Task 1: Companies by year founded -----
+for_plot_T1 = df['Year Founded']
 for_plot_T1.groupby(for_plot_T1).count().plot(kind="bar", color='#ffb81c') # Counts number per year
 sfont = {'fontname':'Arial'}
-plt.xlabel("Year", **sfont, fontsize=14)
+plt.xlabel("Year Founded", **sfont, fontsize=14)
 plt.ylabel("Number of Companies ", **sfont, fontsize=14)
-plt.title("Biotech Industry", **sfont, fontsize=20)
+#plt.title("Biotech Industry", **sfont, fontsize=20)
 plt.tight_layout()
-#plt.show()
 
-
-# Split Public and Private
-for_plot_T2 = result[['Year Founded', 'Company Type']]
-
+# ----- Task 2: Public and Private Companies -----
+for_plot_T2 = df[['Year Founded', 'Company Type']]
 for_plot_T2pr = for_plot_T2[for_plot_T2['Company Type'].str.contains('Pr')]
 for_plot_T2pu = for_plot_T2[for_plot_T2['Company Type'].str.contains('Pu')]
 
-a=for_plot_T2pr.groupby(for_plot_T2pr['Year Founded']).count()
-b=for_plot_T2pu.groupby(for_plot_T2pu['Year Founded']).count()
-
-T2=pd.concat([a, b], axis=1)
+T2 = pd.concat([for_plot_T2pr.groupby(for_plot_T2pr['Year Founded']).count(), 
+                for_plot_T2pu.groupby(for_plot_T2pu['Year Founded']).count()], axis=1)
 
 # Plot for Task 2
 fig, ax = subplots()
@@ -82,9 +68,8 @@ plt.ylabel("Number of Companies ", **sfont, fontsize=14)
 plt.title("Biotech Industry", **sfont, fontsize=20)
 ax.legend(["Private Companies", "Public Companies"])
 plt.tight_layout()
-#plt.show()
 
-# Start Task 3
+# ----- Task 3: Types of Drug -----
 mAbs_HitList = ["mAbs", "monoclonal", "mabs", "monoclonal antibodies"]
 RDNA_HitList = ["RDNA", "R-DNA", "Recombinant", "rDNA", "r-DNA", "recombinant"]
 Antisense_HitList = ["Antisense", "antisense", "3GA"]
@@ -92,6 +77,7 @@ GeneTherapy_HitList = ["Gene Therapy", "gene therapies", "gene therapy", "Gene t
 Chemicals_HitList = ["Chemicals", "chemicals"]
 
 df["All Description"] = df["Business Description"] + df["Long Business Description"] + df["Product Description"] # Possibly need spaces between
+AllDesc_OneString = ' '.join(df['All Description'])
 
 df["is_mAbs"] = [any(x in str for x in mAbs_HitList) for str in df['All Description']]
 df["is_RDNA"] = [any(x in str for x in RDNA_HitList) for str in df['All Description']]
